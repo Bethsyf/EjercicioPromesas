7d3d7be1d0322671269f08bde5d60602b344b20c
Bethsyf <bethsyfalcon@gmail.com>
2022-03-30 17:04:16 -0500

    agregar JS para la presentacion

---
+++ scripts/integrantes.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    let cardBet = document.getElementById("f10");
+    
+        cardBet.innerHTML=`<div class="card" style="width: 18rem; text-align: center;">
+        <img  style="width: 100%;" src="https://res.cloudinary.com/dmaviub4l/image/upload/v1648677306/20220122_132009_aabibx.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h1 class="card-title">Bethsy Falcon</h1>
+          <p class="card-text">Tengo 39 años, he tenido un poco de contacto con la programacion con Python y Java, soy venezolana y actualmente resido en Medellin, me gusta mucho la tecnologia en especial lo relacionado a la web </p>
+          </div>
+        </div>`
+    
+        })
